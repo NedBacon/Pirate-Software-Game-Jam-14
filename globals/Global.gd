e32b0ded7982426c55dc42a003f76924f55d0c01
El Eitch Kei <luishkruger@gmail.com>
2024-01-22 23:48:05 -0300

    Implemented the dialogue system and interaction between player and npc

---
+++ globals/Global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+var trust_score = 0
+var trust_level = 1
+var time_to_spell
+
--- scene/player/player.gd
+++ scene/player/player.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
+
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var facing_direction = $Direction
+@onready var interaction_finder = $Direction/Interaction
+
+
 var input_direction : Vector2 = Vector2.ZERO
 var last_direction : int
 
@@ -12,6 +17,7 @@ func _physics_process(_delta):
 	# Move and Slide function uses velicoty of charcter body to move character on map
 	move_and_slide()
 	update_animation()
+	start_chat()
 	
 func update_animation():
 	if input_direction.x != 0 || input_direction.y != 0:
@@ -28,17 +34,30 @@ func update_facing_direction():
 	if input_direction.x < 0:
 		animated_sprite.flip_h = true
 		animated_sprite.play("Walk_Side")
+		facing_direction.rotation = rad_to_deg(-90.0)
 		last_direction = 1
+		
 	elif input_direction.x > 0:
 		animated_sprite.flip_h = false
 		animated_sprite.play("Walk_Side")
+		facing_direction.rotation = rad_to_deg(90.0)
 		last_direction = 1
+		
 	elif input_direction.y > 0:
 		animated_sprite.flip_h = false
 		animated_sprite.play("Walk_Down")
+		facing_direction.rotation = rad_to_deg(0.0)
 		last_direction = 0
-	else: #input_direction.y < 0:
+		
+	else: 
 		animated_sprite.flip_h = false
 		animated_sprite.play("Walk_Up")
+		facing_direction.rotation = rad_to_deg(180.0)
 		last_direction = 2
 
+func start_chat():
+	if Input.is_action_just_pressed("interaction"):
+		var interactable = interaction_finder.get_overlapping_areas()
+		if interactable.size() > 0:
+			interactable[0].action()
+			return
